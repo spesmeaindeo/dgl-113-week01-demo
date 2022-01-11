4adf9ac54dda6e24201ba75d0df4ddc322ba6a89
Brian Koehler <bkoehler@langara.ca>
2022-01-10 22:32:38 -0800

    fix for demo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,3 +1,4 @@
+import exp from "constants";
 import path from "path";
 
 describe("index.html", () => {
@@ -11,13 +12,37 @@ describe("index.html", () => {
     });
   });
 
+  it("proPrice == 99", async () => {
+    const proPrice = await page.evaluate("proPrice");
+    expect(proPrice).toEqual(99);
+  });
+
+  it("proPrice card 99", async () => {
+    await page.waitForSelector('#propriceh1');
+    const element = await page.$('#propriceh1');
+    const value = await page.evaluate(el => el.textContent, element);
+    expect(value).toMatch(/^\$.*[^0-9\.]99\/mo$/);
+  });
+
+  it("enterprisePrice == 198", async () => {
+    const enterprisePrice = await page.evaluate("enterprisePrice");
+    expect(enterprisePrice).toEqual(198);
+  });
+  
+  it("enterprisePrice card 198", async () => {
+    await page.waitForSelector('#enterprisepriceh1');
+    const element = await page.$('#enterprisepriceh1');
+    const value = await page.evaluate(el => el.textContent, element);
+    expect(value).toMatch(/^\$.*[^0-9\.]198\/mo$/);
+  });
+  
   it("should display a dialog", () => {
     expect(dialogHandler2).toHaveBeenCalled();
   });
 
-  it('should have message "Hello!"', () => {
+  it('should have message "Check out our amazing prices!"', () => {
     const [firstCall] = dialogHandler2.mock.calls;
     const [dialog] = firstCall;
-    expect(dialog.message()).toEqual("Hello!");
+    expect(dialog.message()).toEqual("Check out our amazing prices!");
   });
 });
